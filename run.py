93c73498bdb72f7b630bf1d923fcb861eff2fc37
Jenni Puhakka <jenni.puhakka@aalto.fi>
2024-03-01 15:00:18 +0200

    v2

--- run.py
+++ run.py
@@ -1,6 +1,23 @@
 import numpy as np
 from math import sqrt
 
+
+def kronecker_product(A, B):
+    rows_A, cols_A = len(A), len(A[0])
+    rows_B, cols_B = len(B), len(B[0])
+
+    result = [[0] * (cols_A * cols_B) for _ in range(rows_A * rows_B)]
+
+    for i in range(rows_A):
+        for j in range(cols_A):
+            for k in range(rows_B):
+                for l in range(cols_B):
+                    result[i * rows_B + k][j * cols_B + l] = A[i][j] * B[k][l]
+
+    return result
+
+
+
 # 1. a) Show that H ⊗ H ⊗ H = H^[3|2]H^[3|1]H^[3|0].
 
 ## Definitions
@@ -50,13 +67,19 @@ print("= 1/(2*sqrt(2))*", multiplication_result_common, "\n")
 
 ## Computing the Kronecker product H ⊗ H ⊗ H
 
+# Using numpy
 first_kronecker_product = np.kron(H, H)
-kronecker_product_result = np.kron(H, first_kronecker_product)
+kronecker_product_result = np.kron(first_kronecker_product,H)
+
+# Using direct defenitions in the sake of learning
+first_kronecker_product_definitions = kronecker_product(H, H)
+kronecker_product_result_definitions  = kronecker_product(H, first_kronecker_product_definitions)
 
 kronecker_product_result_common = kronecker_product_result / (1/(2*sqrt(2))) # common factor 1/2 out
 
 print("**LEFT HAND SIDE:\n")
 print("H ⊗ H ⊗ H =",kronecker_product_result, "\n")
+print("Using the definitions - H ⊗ H ⊗ H =",kronecker_product_result_definitions, "\n")
 print("= 1/(2*sqrt(2))*", kronecker_product_result_common)
 
 ## Equality between the right and left hand side
